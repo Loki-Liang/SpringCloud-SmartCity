7ffac248716bf208bb470873c051d301daf1f00a
Rocky_Liang <2683737168@qq.com>
2021-11-15 09:23:24 +0800

    构建基础架构

---
+++ TripData/src/main/java/com/sc/tripdata/TripDataApplication.java
@@ -0,0 +1,13 @@
+package com.sc.tripdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripDataApplication.class, args);
+	}
+
+}
---
+++ TripData/src/test/java/com/sc/tripdata/TripDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sc.tripdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
